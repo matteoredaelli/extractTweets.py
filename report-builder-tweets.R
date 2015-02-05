1666f39737fd54671345acca371496df0b555801
matteoredaelli <matteo.redaelli@gmail.com>
2015-02-05 14:05:50 +0100

    removed about link

--- report-builder-tweets.R
+++ report-builder-tweets.R
@@ -76,7 +76,7 @@ if(include.tweets) {
 
 #M=gvisLineChart(tweets.by.day, xvar="V1", yvar="V2")
 tweets.by.day = tweets.by.day[with(tweets.by.day, order(Item)),]
-tweets.by.day.chart=gvisLineChart(tweets.by.day, options=list(width=400, 
+tweets.by.day.chart=gvisLineChart(tweets.by.day, options=list(width=600, 
                                             height=250,
                                             legend='none',
                                             title="tweets by day"))
